a13f137c58bb45de63e028563c4e12ded950490c
alembikovm@gmail.com <alembikovme@altarix.ru>
2020-10-05 12:01:00 +0400

    [Задачник п.2.1] Рефакторинг

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -1,19 +1,3 @@
-/*
- * Решение 1:
- * Создаем 2 (русский и английский) алфавита и сравниваем
- * является ли подстрока частью алфавита
- */
-let russianAlphabet = "";
-let englishAlphabet = "";
-
-for (let i = 65; i <= 220; i++) {
-  englishAlphabet += String.fromCodePoint(i);
-}
-
-for (let i = 1025; i <= 1105; i++) {
-  russianAlphabet += String.fromCodePoint(i);
-}
-
 /**
  * sortStrings - sorts array of string by two criteria "asc" or "desc"
  * @param {string[]} arr - the array of strings
@@ -27,43 +11,7 @@ export function sortStrings(arr, param = "asc") {
     if (param === "desc") {
       [a, b] = [b, a];
     }
-    if (russianAlphabet.includes(a[0]) && russianAlphabet.includes(b[0])) {
-      return a.localeCompare(b, "ru", { caseFirst: "upper" });
-    }
 
-    if (englishAlphabet.includes(a[0]) && englishAlphabet.includes(b[0])) {
-      return a.localeCompare(b, "en", { caseFirst: "upper" });
-    }
-
-    return a.localeCompare(b, "ru");
+    return a.localeCompare(b, "ru", { caseFirst: "upper" });
   });
 }
-
-// Решение ниже не прошло тесты на стороне github
-// /*
-//  * Решение 2:
-//  * Передаем undefined в locale
-//  * и используем только необходимое нам правило { caseFirst: "upper" }
-//  */
-
-// /**
-//  * sortStrings - sorts array of string by two criteria "asc" or "desc"
-//  * @param {string[]} arr - the array of strings
-//  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
-//  * @returns {string[]}
-//  */
-// export function sortStrings(arr, param = "asc") {
-//   const newArr = [...arr];
-
-//   return newArr.sort((a, b) => {
-//     /*
-//      * Так как Array.reverse() вызовет дополнительный проход по массиву,
-//      * то поменяем переменные местами
-//      */
-//     if (param === "desc") {
-//       [a, b] = [b, a];
-//     }
-
-//     return a.localeCompare(b, undefined, { caseFirst: "upper" });
-//   });
-// }
