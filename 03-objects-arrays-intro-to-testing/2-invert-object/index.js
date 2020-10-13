d381912f356376847e7f0b97826226963b382408
alembikovm@gmail.com <alembikovme@altarix.ru>
2020-10-13 23:21:25 +0400

    [Задачник 3.2] Invert objects

--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,5 +4,15 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
+  const newObj = {};
 
+  if (!obj || !Object.keys(obj).length) {
+    return obj;
+  }
+
+  for (const [key, value] of Object.entries(obj)) {
+    newObj[value] = key;
+  }
+
+  return newObj;
 }
