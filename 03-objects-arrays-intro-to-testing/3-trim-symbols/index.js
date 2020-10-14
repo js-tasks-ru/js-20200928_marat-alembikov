03e5bb9739de18ed05c0d02d920ec347a61826ea
alembikovm@gmail.com <alembikovme@altarix.ru>
2020-10-14 13:49:19 +0400

    [Задачник 3.3] trimSymbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,22 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (size === undefined) {
+    return string;
+  }
 
+  if (size === 0) {
+    return "";
+  }
+
+  let transformedString = "";
+  const symbolsCollection = string.split("");
+
+  for (let symb of symbolsCollection) {
+    if (!transformedString.endsWith(symb.repeat(size))) {
+      transformedString += symb;
+    }
+  }
+
+  return transformedString;
 }
