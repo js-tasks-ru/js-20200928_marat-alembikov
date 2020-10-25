9efab01df2f1061ac5e50764b6ee3f55f1da8a01
alembikovm@gmail.com <alembikovme@altarix.ru>
2020-10-25 16:50:11 +0400

    [Задачник 4.1] Создание виджета ColumnChart

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,95 @@
 export default class ColumnChart {
+  subElements = {};
+  chartHeight = 50;
 
+  constructor({ data = [], label = "", link = "", value = 0 } = {}) {
+    this.data = data;
+    this.label = label;
+    this.link = link;
+    this.value = value;
+
+    this.render();
+  }
+
+  getLink() {
+    return this.link
+      ? `<a class="column-chart__link" href="${this.link}">View all</a>`
+      : "";
+  }
+
+  getColumnBody(data) {
+    const maxValue = Math.max(...data);
+    const scale = this.chartHeight / maxValue;
+
+    return data
+      .map((item) => {
+        const percent = ((item / maxValue) * 100).toFixed(0);
+
+        return `<div style="--value: ${Math.floor(
+          item * scale
+        )}" data-tooltip="${percent}%"></div>`;
+      })
+      .join("");
+  }
+
+  getColmnChartElements() {
+    return `
+    <div class="column-chart column-chart_loading " style="--chart-height: ${
+      this.chartHeight
+    }">
+        <div class="column-chart__title">
+            ${this.label}
+            ${this.getLink()}
+        </div>
+
+        <div class="column-chart__container">
+            <div data-element="header" class="column-chart__header">
+                ${this.value}
+            </div>
+
+            <div data-element="body" class="column-chart__chart">
+                ${this.getColumnBody(this.data)}
+            </div>
+        </div>
+    </div>
+`;
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll("[data-element]");
+
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  render() {
+    const wrap = document.createElement("div");
+
+    wrap.innerHTML = this.getColmnChartElements();
+
+    this.element = wrap.children[0];
+
+    if (this.data.length) {
+      this.element.classList.remove("column-chart_loading");
+    }
+
+    this.subElements = this.getSubElements(this.element);
+  }
+
+  update() {
+    this.subElements.body.innerHTML = this.getColumnBody(this.data);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
+  }
 }
